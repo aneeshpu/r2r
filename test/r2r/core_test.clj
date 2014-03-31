9ef77aa94dd3663da18d02b775430190c897ad18
Aneesh Puliyedath Udumbath <aneesh.udumbath@RACKSPACE.COM>
2014-03-31 15:24:09 +0530

    Added a unit test case

--- test/r2r/core_test.clj
+++ test/r2r/core_test.clj
@@ -1,7 +1,14 @@
 (ns r2r.core-test
   (:require [clojure.test :refer :all]
-            [r2r.core :refer :all]))
+            [r2r.controller :refer :all]
+            [r2r.db.mongo :as r2r-db]))
 
-(deftest a-test
+(comment deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest saves-a-learning-in-db
+  (with-redefs-fn {#'r2r-db/insert (fn [documents learning]
+                                     (is (= "learnings" documents))
+                                     (is (= {:question "test question" :answer "test answer"} learning)))}
+    #(add-learning {:question "test question" :answer "test answer"})))
