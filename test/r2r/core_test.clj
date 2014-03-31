722684e63c63c8ddca16209087741a31aeb5dcbb
Aneesh Puliyedath Udumbath <aneesh.udumbath@RACKSPACE.COM>
2014-03-31 15:25:52 +0530

    Removed a redundant test case

--- test/r2r/core_test.clj
+++ test/r2r/core_test.clj
@@ -3,10 +3,6 @@
             [r2r.controller :refer :all]
             [r2r.db.mongo :as r2r-db]))
 
-(comment deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (deftest saves-a-learning-in-db
   (with-redefs-fn {#'r2r-db/insert (fn [documents learning]
                                      (is (= "learnings" documents))
