08ce1ac3b5021aaefcf307aaf5f64d33bd8c830b
Aneesh Puliyedath Udumbath <aneesh.udumbath@RACKSPACE.COM>
2014-04-05 18:18:40 +0530

    Made the add new learning page a partial

--- resources/public/controllers.js
+++ resources/public/controllers.js
@@ -2,6 +2,13 @@ var r2r = angular.module('r2r', ['ngRoute']);
 
 r2r.config(['$routeProvider', function ($routeProvider) {
 
+    $routeProvider.when('/New', {
+        templateUrl: 'partials/new-learning.html',
+        controller: 'R2RController'
+    }).otherwise({
+            redirectTo: '/New'
+        });
+
 }]);
 
 r2r.controller('R2RController', function ($scope, $http, $location) {
